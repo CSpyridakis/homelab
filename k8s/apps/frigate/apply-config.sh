e170c6f7f8f554a15a01bba9f9f404a15974438a
Spyridakis Christos <spyridakischristos@gmail.com>
2026-09-13 18:06:56 +0300

    Frigate: Rename .yml -> .yaml and add in the kustomization the ns

--- k8s/apps/frigate/apply-config.sh
+++ k8s/apps/frigate/apply-config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Regenerate secret
-kubectl -n applications create configmap frigate-config --from-file config/config.yaml -o yaml --dry-run=client > configmap.yml
-kubectl -n applications create secret generic frigate-secrets --from-env-file config/.env -o yaml --dry-run=client > secret.yml
+kubectl -n applications create configmap frigate-config --from-file config/config.yaml -o yaml --dry-run=client > configmap.yaml
+kubectl -n applications create secret generic frigate-secrets --from-env-file config/.env -o yaml --dry-run=client > secret.yaml
 
 # Apply configmap 
-kubectl apply -f configmap.yml
-kubectl apply -f secret.yml
+kubectl apply -f configmap.yaml
+kubectl apply -f secret.yaml
 
 # Restart pods (not using kubectl rollout restart because the system I have has less than cpu limit)
 if kubectl -n applications get pods --no-headers 2> /dev/null | grep -q frigate ; then
