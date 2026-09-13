57a1ceb253eed8ecf35ab55cb89cd332c5723f99
Spyridakis Christos <spyridakischristos@gmail.com>
2026-09-13 18:10:06 +0300

    Frigate: apply-config updated to have the new ns

--- k8s/apps/frigate/apply-config.sh
+++ k8s/apps/frigate/apply-config.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# TODO: if change ns in the future, need to change it here too
+
 # Regenerate secret
-kubectl -n applications create configmap frigate-config --from-file config/config.yaml -o yaml --dry-run=client > configmap.yaml
-kubectl -n applications create secret generic frigate-secrets --from-env-file config/.env -o yaml --dry-run=client > secret.yaml
+kubectl -n privileged-apps create configmap frigate-config --from-file config/config.yaml -o yaml --dry-run=client > configmap.yaml
+kubectl -n privileged-apps create secret generic frigate-secrets --from-env-file config/.env -o yaml --dry-run=client > secret.yaml
 
 # Apply configmap 
 kubectl apply -f configmap.yaml
 kubectl apply -f secret.yaml
 
 # Restart pods (not using kubectl rollout restart because the system I have has less than cpu limit)
-if kubectl -n applications get pods --no-headers 2> /dev/null | grep -q frigate ; then
-  kubectl -n applications delete pod "$(kubectl -n applications get pods --no-headers | grep frigate | cut -d ' ' -f 1)"
+if kubectl -n privileged-apps get pods --no-headers 2> /dev/null | grep -q frigate ; then
+  kubectl -n privileged-apps delete pod "$(kubectl -n privileged-apps get pods --no-headers | grep frigate | cut -d ' ' -f 1)"
 fi
